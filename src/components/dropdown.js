001cb2224c19284a0f745cceccb15c743080d01c
Mayank-46 <mayank.jaiswal1406@gmail.com>
2022-06-30 12:47:59 +0530

    first

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,55 +1,41 @@
 import React, { useEffect, useState } from 'react'
 import JSONDATA from './../details.json'
 // import filter1 from './filter1';
+let lst=[]
 
-var mango = 2;
+export default function Dropdown() {
 
-export function Dropdown() {
+    // Filter part
+
     const [searchTerm,setSearchTerm] = useState('')
-  function check(name){
-    // return "hello"
-    return(name.laureates ? name.laureates.map((details,ind)=>{
-        // console.log(details.firstname)
-        return(
-            details.firstname
-        )
-    }) : "")
-}
+    function check(name){
+        //     for(let i=0;i<658;i++){
+        //        for(let j=0;j<name.laureates.length;j++){
+        //         lst.push(name[0].laureates[0].firstname)
+                   return(name.laureates ? name.laureates.map((details,ind)=>{
+                    return(
+                        (details.firstname +' '+ details.surname+',')
+                 )
+                 }) : "")
+        //      }
 
-    const [category,setCategory] = useState("")
-
-
-    useEffect(() => {
-
-        console.log("the current category is :" + category)
-        // setSearchTerm=category;
-        // {JSONDATA.prizes.filter((val)=>{
-        //     if(searchTerm === ""){
-        //       return val
-        //     }
-        //     else if(val.category.toLowerCase().includes(searchTerm.toLowerCase())){
-        //       return val;
-        //     }
-        //   }).map((name,key)=>{
-        //     return <div>
-        //     <div className="card">
-        //     <h2 className="mx-2 my-2">Prize : {name.category} </h2> 
-        //     <h2 className="my-2 py-2">Year : {name.year} </h2>
-        //     <h2> firstName : {check(name)} </h2>
-        //     </div>
-        // </div>
-        //   })}
-
-
-    }, [category])
-    
-
-
-
-const mySet1 = new Set()
-let mySet2;
+        // }
+        console.log(lst)
         
-        console.log(JSONDATA.prizes)
+            
+       }
+
+
+
+    const mySet1 = new Set()
+    let mySet2;
+    const years = [];
+    let year= 1900;
+    for (var i =0; i < 119; i++) {
+            years[i]=(year+i)
+            // console.log(year+i)
+        }
+        
         for (let i = 0; i < JSONDATA.prizes.length; i++) {
             mySet1.add(JSONDATA.prizes[i].category)
           }
@@ -62,29 +48,69 @@ let mySet2;
 
 
   return (
-    <div>
-    <div class="input-group mb-3">
-    <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Category</button>
-    <ul class="dropdown-menu">
+
+    <>
+    {/* Dropdown Part */}
+    <h2 className='Heading'>Nobel Prize Winners List</h2>
+
+    <div className='header'>
+    <div className='box' >
+    <div class="input-group mb-3 ">
+    <button class="button btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Category</button>
+    <ul class="dropdown-menu ">
         {/* <li><a class="dropdown-item" href="#">{mySet2[0]}</a></li> */}
     {mySet2.map((value)=>{
         return (
-            <><li><a class="dropdown-item" href="#"  onClick={()=>setCategory(value)}>{value}</a></li><li><hr class="dropdown-divider" /></li></>)
+            <><li><a class="dropdown-item" href="#"  onClick={()=>setSearchTerm(value)}>{value}</a></li><li><hr class="dropdown-divider" /></li></>)
     }
     )}
       
     </ul>
   </div>
-  <div class="input-group mb-3">
+  
+  <div class="input-group mb-3 ">
     <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Year</button>
-    <ul class="dropdown-menu">
-
-      <li><a class="dropdown-item" href="#">Category</a></li>
-      <li><hr class="dropdown-divider"/></li>
+    <ul class="dropdown-menu yearList">
+    {years.map((value)=>{
+        return (
+            <><li><a class="dropdown-item" href="#"  onClick={()=>setSearchTerm(value)}>{value}</a></li><li><hr class="dropdown-divider" /></li></>)
+    }
+    )}
     </ul>
   </div>
+  <div class="input-group mb-3">
+    <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" onClick={()=>setSearchTerm("")}>All</button>
   </div>
-  )
-}
+  </div>
+  </div>
 
-export default mango
+
+{/* Filter Part */}
+
+        {/* {filter} */}
+        {JSONDATA.prizes.filter((val,key)=>{
+            if(searchTerm === ""){
+              return val
+            }
+            else if(typeof(searchTerm)=="string" && val.category.toLowerCase().includes(searchTerm.toLowerCase())){
+              return val;
+            }
+            else if(typeof(searchTerm)=="number" && val.year.includes(searchTerm)){
+                return val;
+            }
+          }).map((name,key)=>{
+            return <div>
+            <div className="card">
+            <h2 className="mx-2 my-2">Prize Category : {name.category} </h2> 
+            <h2 className="my-2 py-2">Year : {name.year} </h2>
+            <h2> Name : {check(name)} </h2>
+            </div>
+        </div>
+          })}
+          
+
+  </>        
+
+  )
+
+}
--- src/components/prize.jsx
+++ src/components/prize.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-// import { useState } from 'react';
-// import JSONDATA from "../JSONDATA.json"
-// import JSONDATA from "../"
+
 import JSONDATA from "../details.json"
 import "./prize.css"
 import Filter from './filter'
-import {Dropdown} from './dropdown'
+import Dropdown from './dropdown'
 
 
 
@@ -15,34 +13,10 @@ import {Dropdown} from './dropdown'
 
 export default function Prize() {
 
-    // function check(name){
-    //     // return "hello"
-    //     return(name.laureates ? name.laureates.map((details,ind)=>{
-    //         // console.log(details.firstname)
-    //         return(
-    //             details.firstname
-    //         )
-    //     }) : "")
-    // }
-
-    // const content = JSONDATA.prizes
+    
   return (
     <div>
     <Dropdown />
-    <Filter />
-    {/* {content.map((name, ind)=>{
-        return(
-            <>
-            <div className="card">
-
-
-            <h2 className="mx-2 my-2">Prize : {name.category} </h2> 
-            <h2 className="my-2 py-2">Year : {name.year} </h2>
-            <h2> firstName : {check(name)} </h2>
-            </div>
-            </>
-        )
-    })} */}
     
     </div>
   )
